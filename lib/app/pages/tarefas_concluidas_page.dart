19a812a7f8171d9c44910237fdfa68189df5048d
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2021-08-25 19:48:11 -0300

    refactor: substitui métodos de criação pela utilização das classes de widgets criados

--- lib/app/pages/tarefas_concluidas_page.dart
+++ lib/app/pages/tarefas_concluidas_page.dart
@@ -1,26 +1,19 @@
-import 'package:app_flutter_tarefas/app/models/disciplina_model.dart';
+import 'package:app_flutter_tarefas/app/components/botao_circular.dart';
+import 'package:app_flutter_tarefas/app/components/texts_info_tarefas.dart';
+import 'package:app_flutter_tarefas/app/controllers/disciplina_controller.dart';
 import 'package:app_flutter_tarefas/app/models/tarefa_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class TarefaConcluidaPage extends StatelessWidget {
+class TarefaConcluidaPage extends StatefulWidget {
   TarefaConcluidaPage({Key? key}) : super(key: key);
 
-  List<Tarefa> tarefas = [
-    Tarefa(
-      'Fazer diagrama de classes',
-      DateTime.now(),
-      Disciplina('POO', 'relens'),
-      false,
-    ),
-    Tarefa(
-      'Entrega 2 - Criar casos de teste',
-      DateTime.now(),
-      Disciplina('RP 2', 'testes'),
-      false,
-    ),
-  ];
+  @override
+  _TarefaConcluidaPageState createState() => _TarefaConcluidaPageState();
+}
 
+class _TarefaConcluidaPageState extends State<TarefaConcluidaPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,103 +26,7 @@ class TarefaConcluidaPage extends StatelessWidget {
           child: Center(
             child: Column(
               children: [
-                ListView.builder(
-                  itemCount: tarefas.length,
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(
-                        left: 40,
-                        right: 40,
-                        top: 10,
-                        bottom: 10,
-                      ),
-                      child: Container(
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          color: Colors.purple[100],
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding:
-                                    const EdgeInsets.only(top: 20, left: 20),
-                                child: Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Text(
-                                    tarefas[index].getDescricao(),
-                                    style: TextStyle(
-                                      color: Colors.deepPurple[700],
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.only(top: 20, left: 20),
-                                child: Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Text(
-                                    'Disciplina: ' +
-                                        tarefas[index].getDisciplina().nome,
-                                    style: TextStyle(
-                                      color: Colors.deepPurple[900],
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.normal,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.only(top: 20, left: 20),
-                                child: Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Text(
-                                    'Entrega: ' +
-                                        tarefas[index]
-                                            .getDataEntrega()
-                                            .toString(),
-                                    style: TextStyle(
-                                      color: Colors.deepPurple[900],
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.normal,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.only(top: 20, left: 20),
-                                child: Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Row(
-                                    children: [
-                                      Icon(Icons.check,color: Colors.green,size: 30,),
-                                      Text(
-                                        'Concluída',
-                                        style: TextStyle(
-                                          color: Colors.deepPurple[900],
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.normal,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              getButtonsCardTarefa(),
-                            ],
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
+                _getListViewBuilder(),
                 SizedBox(
                   height: 60,
                 ),
@@ -141,7 +38,71 @@ class TarefaConcluidaPage extends StatelessWidget {
     );
   }
 
-  getButtonsCardTarefa() {
+  _getListViewBuilder() {
+    DisciplinaController dc =
+        Provider.of<DisciplinaController>(context, listen: false);
+    return ListView.builder(
+      itemCount: dc.getTarefasConcluidas().length,
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(
+            left: 40,
+            right: 40,
+            top: 10,
+            bottom: 10,
+          ),
+          child: Container(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              color: Colors.purple[100],
+              child: Column(
+                children: [
+                  TextInfoTarefa(
+                    tarefa: dc.getTarefasConcluidas().elementAt(index),
+                  ),
+                  _getTextConcluida(),
+                  _getButtonsCardTarefa(
+                    dc.getTarefasConcluidas().elementAt(index),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+  _getTextConcluida() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, left: 20),
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Row(
+          children: [
+            Icon(
+              Icons.check,
+              color: Colors.green,
+              size: 30,
+            ),
+            Text(
+              'Concluída',
+              style: TextStyle(
+                color: Colors.deepPurple[900],
+                fontSize: 20,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _getButtonsCardTarefa(Tarefa tarefa) {
     return Padding(
       padding: EdgeInsets.only(
         bottom: 6,
@@ -152,22 +113,18 @@ class TarefaConcluidaPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           SizedBox(width: 10),
-          createButtonCardTarefa(Icons.delete, () {}),
+          BotaoCircular(
+            icon: Icons.delete,
+            onPressed: () {
+              setState(() {
+                DisciplinaController dc =
+                    Provider.of<DisciplinaController>(context, listen: false);
+                dc.excluirTarefa(tarefa);
+                dc.notifyListeners();
+              });
+            },
+          ),
         ],
-      ),
-    );
-  }
-
-  createButtonCardTarefa(IconData icon, void Function() onPressed) {
-    return Ink(
-      decoration: const ShapeDecoration(
-        color: Colors.deepPurple,
-        shape: CircleBorder(),
-      ),
-      child: IconButton(
-        icon: Icon(icon),
-        color: Colors.white,
-        onPressed: onPressed,
       ),
     );
   }
